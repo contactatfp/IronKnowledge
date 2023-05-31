d2114be77e86710178d8fdc51b00d6ae0bd4bc03
Kyle Louderback <85082344+contactatfp@users.noreply.github.com>
2023-05-31 10:10:42 -0700

    Update config.py

--- IronKnowledge/config.py
+++ IronKnowledge/config.py
@@ -8,11 +8,6 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     GPT4_API_KEY = os.environ.get('GPT4_API_KEY')
     HR_API_KEY = os.environ.get('HR_API_KEY') or 'your-hr-api-key-here'
-    # MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
-    # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
-    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or True
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'contact@fakepicasso.com'
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'itsnotreal00'
     # ADMINS = ['kyle@example.com']
     GMAIL_SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
     EMBEDDING_MODEL = "text-embedding-ada-002"
