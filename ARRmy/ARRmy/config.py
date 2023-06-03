f255992c10c5a7ce5de61337f445ac01153ffd87
Kyle Louderback <85082344+contactatfp@users.noreply.github.com>
2023-06-02 21:48:22 -0700

    Delete

--- ARRmy/ARRmy/config.py
+++
@@ -1,6 +0,0 @@
-# import os
-#
-# class Config:
-#     SECRET_KEY = os.urandom(16).hex()
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'database/site.db')
